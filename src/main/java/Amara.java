af65a474a9d5e9da9a697389b8f0190945b722f3
ernestnzx <ernestngzixuan@gmail.com>
2025-01-18 02:45:21 +0800

    Level-0

---
+++ src/main/java/Amara.java
@@ -0,0 +1,18 @@
+public class Amara {
+    private static final String BOARDER = "_".repeat(60);
+
+    Amara() {
+    }
+
+    private String wrapText(String text) {
+        return String.format("%s\n%s\n%s", Amara.BOARDER, text, Amara.BOARDER);
+    }
+
+    String greet() {
+        return this.wrapText("Hello I'm Amara\nWhat can I do for you?");
+    }
+
+    String exit() {
+        return this.wrapText("Bye. Hope to see you again soon!");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Amara amara = new Amara();
+        System.out.println(amara.greet() + "\n" + amara.exit());
+    }
+}
