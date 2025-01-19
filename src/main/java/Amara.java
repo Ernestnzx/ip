acd4919aef1f0d357d830b74f8caf1fa7129736d
ernestnzx <ernestngzixuan@gmail.com>
2025-01-19 10:38:56 +0800

    Moved logic from Main to Amara Bot

--- src/main/java/Amara.java
+++ src/main/java/Amara.java
@@ -1,49 +1,53 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Amara {
     private ArrayList<Task> tasks;
+    private Scanner scanner;
 
     private static final String BOARDER = "=".repeat(60);
 
     Amara() {
         this.tasks = new ArrayList<Task>();
+        this.scanner = new Scanner(System.in);
     }
 
     private String wrapText(String text) {
         return String.format("%s\n%s\n%s", Amara.BOARDER, text, Amara.BOARDER);
     }
 
-    String greet() {
+    private String greet() {
         return this.wrapText("Hello I'm Amara\nWhat can I do for you?");
     }
 
-    String exit() {
+    private String exit() {
         return this.wrapText("Bye. Hope to see you again soon! <3");
     }
 
-    String echo(String sentence) {
+    @SuppressWarnings("unused")
+    private String echo(String sentence) {
         return this.wrapText(sentence);
     }
 
-    String addToList(Task task) {
+    private String addToList(Task task) {
         this.tasks.add(task);
         return this.wrapText(String.format("Got it. I've added this task:\n"
                 + "  %s\nNow you have %d tasks in the list.", task, this.tasks.size()));
     }
 
-    String markTask(int index) {
+    private String markTask(int index) {
         Task task = this.tasks.get(index - 1);
         task.markTask();
         return this.wrapText(String.format("Nice! I've marked this task as done:\n  %s", task));
     }
 
-    String unmarkTask(int index) {
+    private String unmarkTask(int index) {
         Task task = this.tasks.get(index - 1);
         task.unmarkTask();
         return this.wrapText(String.format("OK, I've marked this task as not done yet:\n  %s", task));
     }
 
-    String getList() {
+    private String getList() {
         // Using StringBuilder if speed is needed.
         String taskList = "Here are the tasks in your list:\n";
         int listSize = this.tasks.size();
@@ -54,5 +58,38 @@ public class Amara {
             }
         }
         return this.wrapText(taskList);
+    }
+
+    public void start() {
+        boolean isExit = false;
+        System.out.println(this.greet());
+        while (!isExit) {
+            String reply = "";
+            String command = scanner.nextLine();
+            String commandString = getFirstWord(command);
+            String commandParams = removeFirstWord(command);
+            if (commandString.equalsIgnoreCase("bye")) {
+                reply = this.exit();
+                isExit = true;
+            } else if (commandString.equalsIgnoreCase("list")) {
+                reply = this.getList();
+            } else if (commandString.equalsIgnoreCase("mark")) {
+                reply = this.markTask(Integer.parseInt(commandParams));
+            } else if (commandString.equalsIgnoreCase("unmark")) {
+                reply = this.unmarkTask(Integer.parseInt(commandParams));
+            } else {
+                reply = this.addToList(TaskFactory.getTask(commandString, commandParams));
+            }
+            System.out.println(reply);
+        }
+        scanner.close();
+    }
+
+    private static String getFirstWord(String userInput) {
+        return userInput.trim().split("\\s+")[0];
+    }
+
+    private static String removeFirstWord(String userInput) {
+        return userInput.replace(getFirstWord(userInput), "").trim();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Amara amara = new Amara();
-        boolean isExit = false;
-        System.out.println(amara.greet());
-        while (!isExit) {
-            String reply = "";
-            String command = scanner.nextLine();
-            String commandString = getFirstWord(command);
-            String commandParams = removeFirstWord(command);
-            if (commandString.equalsIgnoreCase("bye")) {
-                reply = amara.exit();
-                isExit = true;
-            } else if (commandString.equalsIgnoreCase("list")) {
-                reply = amara.getList();
-            } else if (commandString.equalsIgnoreCase("mark")) {
-                reply = amara.markTask(Integer.parseInt(commandParams));
-            } else if (commandString.equalsIgnoreCase("unmark")) {
-                reply = amara.unmarkTask(Integer.parseInt(commandParams));
-            } else {
-                reply = amara.addToList(TaskFactory.getTask(commandString, commandParams));
-            }
-            System.out.println(reply);
-        }
-        scanner.close();
-    }
-
-    static String getFirstWord(String userInput) {
-        return userInput.trim().split("\\s+")[0];
-    }
-
-    static String removeFirstWord(String userInput) {
-        return userInput.replace(getFirstWord(userInput), "").trim();
+        amara.start();
     }
 }
